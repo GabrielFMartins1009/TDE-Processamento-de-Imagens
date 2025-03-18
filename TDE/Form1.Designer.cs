2435b9409ccd865e1c7e4aa216220b2bf43b88aa
GabrielFMartins1009 <103799@aluno.uricer.edu.br>
2025-03-17 23:51:32 -0300

    Commit 2

--- TDE/Form1.Designer.cs
+++ TDE/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
+            groupBoxCMYK = new GroupBox();
+            textBox1 = new TextBox();
             buttonCONVERTER = new Button();
             label3 = new Label();
             label2 = new Label();
@@ -42,17 +44,24 @@
             trackBarRED = new TrackBar();
             panelColor = new Panel();
             tabPage2 = new TabPage();
-            groupBoxCMYK = new GroupBox();
-            textBox1 = new TextBox();
+            groupBox1 = new GroupBox();
+            textBox2 = new TextBox();
+            groupBox2 = new GroupBox();
+            textBox3 = new TextBox();
+            groupBox3 = new GroupBox();
+            textBox4 = new TextBox();
             tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
+            groupBoxCMYK.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)numericUpDownBLUE).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDownGREEN).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDownRED).BeginInit();
             ((System.ComponentModel.ISupportInitialize)trackBarBLUE).BeginInit();
             ((System.ComponentModel.ISupportInitialize)trackBarGREEN).BeginInit();
             ((System.ComponentModel.ISupportInitialize)trackBarRED).BeginInit();
-            groupBoxCMYK.SuspendLayout();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
             SuspendLayout();
             // 
             // tabControl1
@@ -62,11 +71,14 @@
             tabControl1.Location = new Point(12, 12);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(586, 546);
+            tabControl1.Size = new Size(662, 576);
             tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
+            tabPage1.Controls.Add(groupBox2);
+            tabPage1.Controls.Add(groupBox3);
+            tabPage1.Controls.Add(groupBox1);
             tabPage1.Controls.Add(groupBoxCMYK);
             tabPage1.Controls.Add(buttonCONVERTER);
             tabPage1.Controls.Add(label3);
@@ -82,10 +94,27 @@
             tabPage1.Location = new Point(4, 24);
             tabPage1.Name = "tabPage1";
             tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(578, 518);
+            tabPage1.Size = new Size(654, 548);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "tabPage1";
             tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxCMYK
+            // 
+            groupBoxCMYK.Controls.Add(textBox1);
+            groupBoxCMYK.Location = new Point(9, 295);
+            groupBoxCMYK.Name = "groupBoxCMYK";
+            groupBoxCMYK.Size = new Size(192, 57);
+            groupBoxCMYK.TabIndex = 13;
+            groupBoxCMYK.TabStop = false;
+            groupBoxCMYK.Text = "CMYK";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(13, 23);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(165, 23);
+            textBox1.TabIndex = 0;
             // 
             // buttonCONVERTER
             // 
@@ -207,42 +236,82 @@
             tabPage2.Text = "tabPage2";
             tabPage2.UseVisualStyleBackColor = true;
             // 
-            // groupBoxCMYK
+            // groupBox1
             // 
-            groupBoxCMYK.Controls.Add(textBox1);
-            groupBoxCMYK.Location = new Point(9, 295);
-            groupBoxCMYK.Name = "groupBoxCMYK";
-            groupBoxCMYK.Size = new Size(192, 57);
-            groupBoxCMYK.TabIndex = 13;
-            groupBoxCMYK.TabStop = false;
-            groupBoxCMYK.Text = "CMYK";
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Location = new Point(9, 358);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(192, 57);
+            groupBox1.TabIndex = 14;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "CMYK";
             // 
-            // textBox1
+            // textBox2
             // 
-            textBox1.Location = new Point(13, 23);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(165, 23);
-            textBox1.TabIndex = 0;
+            textBox2.Location = new Point(13, 23);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(165, 23);
+            textBox2.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(textBox3);
+            groupBox2.Location = new Point(9, 484);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(192, 57);
+            groupBox2.TabIndex = 16;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "CMYK";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(13, 23);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(165, 23);
+            textBox3.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(textBox4);
+            groupBox3.Location = new Point(9, 421);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(192, 57);
+            groupBox3.TabIndex = 15;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "CMYK";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(13, 23);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(165, 23);
+            textBox4.TabIndex = 0;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(610, 570);
+            ClientSize = new Size(681, 605);
             Controls.Add(tabControl1);
             Name = "Form1";
             Text = "Form1";
             tabControl1.ResumeLayout(false);
             tabPage1.ResumeLayout(false);
             tabPage1.PerformLayout();
+            groupBoxCMYK.ResumeLayout(false);
+            groupBoxCMYK.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)numericUpDownBLUE).EndInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDownGREEN).EndInit();
             ((System.ComponentModel.ISupportInitialize)numericUpDownRED).EndInit();
             ((System.ComponentModel.ISupportInitialize)trackBarBLUE).EndInit();
             ((System.ComponentModel.ISupportInitialize)trackBarGREEN).EndInit();
             ((System.ComponentModel.ISupportInitialize)trackBarRED).EndInit();
-            groupBoxCMYK.ResumeLayout(false);
-            groupBoxCMYK.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -264,5 +333,11 @@
         private Button buttonCONVERTER;
         private GroupBox groupBoxCMYK;
         private TextBox textBox1;
+        private GroupBox groupBox2;
+        private TextBox textBox3;
+        private GroupBox groupBox3;
+        private TextBox textBox4;
+        private GroupBox groupBox1;
+        private TextBox textBox2;
     }
 }
